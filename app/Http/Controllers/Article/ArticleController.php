e17f10f2bde18b370d34a10459558898a0209e2a
ykk <yangkuokuo@glz8.com>
2018-05-30 10:12:39 +0800

    commit

--- app/Http/Controllers/Article/ArticleController.php
+++ app/Http/Controllers/Article/ArticleController.php
@@ -127,7 +127,7 @@ class ArticleController extends Controller
         }
     }
 
-    public function del(Request $del){
+    public function del(Request $request){
         $id = $request->input('id');
         $result = Article::where('id',$id)->delete();
         if($result){
