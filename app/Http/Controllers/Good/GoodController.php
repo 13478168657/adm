028766a7b9860120a59b70b69072b8ff2138b738
ykk <yangkuokuo@glz8.com>
2018-06-03 00:06:47 +0800

    commit

--- app/Http/Controllers/Good/GoodController.php
+++ app/Http/Controllers/Good/GoodController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Good;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Good;
+use App\Utils\ImageUpload;
 class GoodController extends Controller
 {
     /**
